1a0f2392e1d4624f95ff2fbbae468186aa7ec0fb
gracelewiss <gracelewis0202@gmail.com>
2022-04-08 22:16:25 +0530

    upload project

---
+++ android/app/src/main/java/com/example/SafeU_Login/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.SafeU_Login;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/welcome_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        leading: null,
+        title: Text('SafeU'),
+        backgroundColor: Colors.red[900],
+      ),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                RoundedButton(
+                  colour: Colors.red[800],
+                  title: 'Log In',
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'login_screen');
+                  },  
+                ),
+                RoundedButton(
+                    colour: Colors.red[800],
+                    title: 'Register',
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'registration_screen');
+                    }),
+              ]),
+        ));
+  }
+}
